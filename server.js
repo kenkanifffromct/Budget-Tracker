56acb0f2921e2eba628f6320f6e096b42778b473
Kenneth Keller <kenkanifffromct21@gmail.com>
2021-04-06 19:35:25 -0400

    updated server file

--- server.js
+++ server.js
@@ -15,15 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/budgets',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budgets", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
 
 // routes
 app.use(require("./routes/api.js"));
